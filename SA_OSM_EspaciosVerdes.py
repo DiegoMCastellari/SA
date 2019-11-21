6d4e0ff0a5bbf366c49726d79bdea5415f291563
DiegoCastellari <agrimcastellari@gmail.com>
2019-11-21 18:23:05 -0300

    Indice - Modificación Función - Limpieza de código
    Se generó un índice para recorrer mejor la notebook.
    Se modificó la función de generación de puntos sobre el perímetro,
    agregandole el parámetro de perímetro mínimo y como resultado que
    devuelva un gdf y no un df.
    Se limpió un poco el código para que quedara mejor: se eliminaron líneas
    innecesarias, se agregaro algunas líneas para definir los crs de los
    gdf en el caso que los procesos no los definieran.

--- SA_OSM_EspaciosVerdes.py
+++ SA_OSM_EspaciosVerdes.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# # <center>Procesamiento de EPV bajado de OSM
+
+# #### Procesamiento de datos de Parques y Reservas Naturalez bajadas de OSM. 
+# #### El proceso devuelve un shp con los puntos sobre el périmetro de los polígonos o en el caso de los polígonos pequeños su centroide (ambos parámetros se definen en la función del punto "Generación de los puntos sobre Perímetro"), para luego ser utilizados para generar el archivo para las consultas a la API de Google.
+# #### Si los datos están guardados correctamente (nombre y carpeta), solamente cambiando el nombre de la ciudad en "Definir ciudad y paths", las direcciones para cargar y exportar los datos se modifican, y todo el proceso corre de punta a punta.
+# 
+# <a id='indice'></a>
+# ## Indice
+#   
+# <a href='#section1'>**Punto 1:**</a>   Definir ciudad y paths   
+# <a href='#section2'>**Punto 2:**</a>   Carga de datos   
+# <a href='#section3'>**Punto 3:**</a>   Proceso de datos de OSM   
+# <a href='#section4'>**Punto 4:**</a>   Poligonos a Linea de Perímetro   
+# <a href='#section5'>**Punto 5:**</a>   Generación de los puntos sobre Perímetro   
+# <a href='#section6'>**Punto 6:**</a>   Calcular distancias más cercana   
+
+# In[15]:
 
 
 import requests
@@ -24,106 +40,119 @@ sys.path.insert(1, 'D:/JOB/Python_Functions/')
 import anapos
 
 
-# # Definir ciudad y paths
+# <a id='section1'></a>
+# 
+# <div style="border-bottom:1px solid #000;"> 
+# <div align="center"><h2>Definir ciudad y paths</h2></div>
+# <div align="right">(volver a <a href='#indice'><b>Indice</b></a>)</div>
+# </div>
 
-# In[2]:
+# In[9]:
 
 
 # Ciudad de Estudio
 Ciudad = 'BuenosAires'
 
 # Path general de la Carpeta (donde se guardarán los resultados)
-PATH = 'D:/JOB/SA/EPV_Ciudades/EPV_BuenosAires/'
+PATH = 'D:/JOB/SA/EPV_Ciudades/EPV_'+ Ciudad + '/'
 
 # Path a los distintos SHPs
-Path_AreaInteres = PATH + 'AreaInteres/Conurbano_2D.shp'
+Path_AreaInteres = PATH + 'AreaInteres/' + Ciudad + '.shp'
 Path_Parques = 'D:/JOB/SA/SHP/OSM/EspacioVerdePúblico/OSM_Parks.shp'
 Path_Reservas = 'D:/JOB/SA/SHP/OSM/EspacioVerdePúblico/OSM_NatureReserve.shp'
-Path_Radios = PATH + 'Radios BA/radios_zonas_amba.shp'
-Path_Zonas = PATH + 'Radios BA/zonas_amba.shp'
+Path_Radios = PATH + 'Radios/Radios_' + Ciudad + '.shp'
+Path_Zonas = PATH + 'Radios/Zonas_' + Ciudad + '.shp'
 
 
-# # Carga de datos
+# <a id='section2'></a>
+# 
+# <div style="border-bottom:1px solid #000;"> 
+# <div align="center"><h2>Carga de datos</h2></div>
+# <div align="right">(volver a <a href='#indice'><b>Indice</b></a>)</div>
+# </div>
 
-# In[3]:
+# In[10]:
 
 
 AreaInteres = gpd.read_file(Path_AreaInteres)
 AreaInteres.head(3)
 
 
-# In[4]:
+# In[11]:
 
 
 Parques = gpd.read_file(Path_Parques)
 Parques.head(3)
 
 
-# In[5]:
+# In[12]:
 
 
 Reservas = gpd.read_file(Path_Reservas)
 Reservas.head(3)
 
 
-# In[6]:
-
-
-Zonas = gpd.read_file(Path_Zonas)
-Zonas.head(3)
-
-
-# # Parques
-
-# In[7]:
-
-
-Parque_Conurbano = gpd.overlay(Parques, AreaInteres, how='intersection')
-Parque_Conurbano.head(3)
-
-
-# In[9]:
-
-
-# conda install -c conda-forge descartes
-
-
-# In[10]:
-
-
-fig, ax = plt.subplots(figsize=(15,15))
-AreaInteres.plot(ax=ax, color='lightgrey')
-Parque_Conurbano.plot(ax=ax, color='green');
-
-
-# # Reserva Natural
-
-# In[11]:
-
-
-Reservas_Conurbano = gpd.overlay(Reservas, AreaInteres, how='intersection')
-Reservas_Conurbano.head(3)
-
-
-# In[12]:
-
-
-fig, ax = plt.subplots(figsize=(15,15))
-AreaInteres.plot(ax=ax, color='lightgrey')
-Parque_Conurbano.plot(ax=ax, color='green')
-Reservas_Conurbano.plot(ax=ax, color='yellow');
-
-
-# # Unimos Parques y Reservas
-
 # In[13]:
 
 
-EspaciosVerdes = gpd.GeoDataFrame(pd.concat([Parque_Conurbano, Reservas_Conurbano], ignore_index=True))
+Zonas = gpd.read_file(Path_Zonas)
+Zonas.crs = {'init' :'epsg:4326'}
+Zonas.head(3)
+
+
+# <a id='section3'></a>
+# 
+# <div style="border-bottom:1px solid #000;"> 
+# <div align="center"><h2>Proceso de datos de OSM</h2></div>
+# <div align="right">(volver a <a href='#indice'><b>Indice</b></a>)</div>
+# </div>
+
+# ### Parques
+
+# In[ ]:
+
+
+Parque_Area = gpd.overlay(Parques, AreaInteres, how='intersection')
+Parque_Area.head(3)
+
+
+# In[ ]:
+
+
+fig, ax = plt.subplots(figsize=(15,15))
+AreaInteres.plot(ax=ax, color='lightgrey')
+Parque_Area.plot(ax=ax, color='green');
+
+
+# ### Reserva Natural
+
+# In[ ]:
+
+
+Reservas_Area = gpd.overlay(Reservas, AreaInteres, how='intersection')
+Reservas_Area.head(3)
+
+
+# In[ ]:
+
+
+fig, ax = plt.subplots(figsize=(15,15))
+AreaInteres.plot(ax=ax, color='lightgrey')
+Parque_Area.plot(ax=ax, color='green')
+Reservas_Area.plot(ax=ax, color='yellow');
+
+
+# ### Unimos Parques y Reservas
+
+# In[ ]:
+
+
+EspaciosVerdes = gpd.GeoDataFrame(pd.concat([Parque_Area, Reservas_Area], ignore_index=True, sort=False))
+EspaciosVerdes.crs = {'init' :'epsg:4326'}
 EspaciosVerdes.head(3)
 
 
-# In[14]:
+# In[ ]:
 
 
 fig, ax = plt.subplots(figsize=(15,15))
@@ -131,53 +160,67 @@ AreaInteres.plot(ax=ax, color='lightgrey')
 EspaciosVerdes.plot(ax=ax, color='green');
 
 
-# In[15]:
+# In[ ]:
 
 
 EspaciosVerdes.to_file(PATH + '/EspaciosVerdes_' + Ciudad + '.shp')
 
 
-# # Poligonos de Espacios Verdes a Linea
+# <a id='section4'></a>
+# 
+# <div style="border-bottom:1px solid #000;"> 
+# <div align="center"><h2>Poligonos de Espacios Verdes a Linea</h2></div>
+# <div align="right">(volver a <a href='#indice'><b>Indice</b></a>)</div>
+# </div>
 
-# In[16]:
+# In[ ]:
 
 
 # Generamos un gdf los EVP como líneas
 EspaciosVerdes_lineas = pd.DataFrame(EspaciosVerdes.boundary)
 EspaciosVerdes_lineas.rename(columns={ EspaciosVerdes_lineas.columns[0]: "geometry" }, inplace = True)
-
 EspaciosVerdes_lineas_GDF = gpd.GeoDataFrame(EspaciosVerdes_lineas, geometry='geometry')
+EspaciosVerdes_lineas_GDF.crs = {'init' :'epsg:4326'}
 
 
-# In[17]:
+# In[ ]:
 
 
 fig, ax = plt.subplots(figsize=(15,15))
 AreaInteres.plot(ax=ax, color='lightgrey')
+EspaciosVerdes.plot(ax=ax, color='yellow')
 EspaciosVerdes_lineas_GDF.plot(ax=ax, color='green');
 
 
-# In[16]:
+# In[ ]:
 
 
 # Guardamos el gdf como SHP
 EspaciosVerdes_lineas_GDF.to_file(PATH + '/EspaciosVerdes_lineas_' + Ciudad + '.shp')
 
 
-# # Puntos sobre Perímetro de Polígonos
+# <a id='section5'></a>
+# 
+# <div style="border-bottom:1px solid #000;"> 
+# <div align="center"><h2>Generación de los puntos sobre Perímetro</h2></div>
+# <div align="right">(volver a <a href='#indice'><b>Indice</b></a>)</div>
+# </div>
 
-# In[18]:
+# ### Puntos sobre Perímetro de Polígonos
+
+# In[ ]:
 
 
 # Función que devuelve puntos sobre las líneas a una distancia especificada. Si el perímetro es inferior al doble de 
 # esta distancia, se calcula el centroide de la línea.-
 
-def Lineas_a_Puntos(DataOriginal, Distancia_entre_puntos):
+def Lineas_a_Puntos_Intervalo(DataOriginal, Dist_puntos, Dist_min_perimetro):
+     
+    Div = Dist_puntos * 1.0457242224755355e-05     # distancia aprox en grados
+    DMP = Dist_min_perimetro * 1.0457242224755355e-05    # distancia aprox en grados
     
-    Inter = Distancia_entre_puntos
-    Div = Inter * 1.0457242224755355e-05     # distancia en grados
 
-    DO_Inter = DataOriginal.loc[DataOriginal.Dist > 2*Div]   # lineas con una longitud mayor a Distancia_entre_puntos
+    DO_Inter = DataOriginal.loc[DataOriginal.Dist > DMP]   # lineas con una longitud mayor a Distancia_entre_puntos
     DO_Inter.reset_index(inplace=True)                     # se resetean los índices para que no haya saltos y corra el for
 
     DF = pd.DataFrame()    # se genera un df vacío donde almacenar las coordenadas de los puntos
@@ -195,96 +238,59 @@ def Lineas_a_Puntos(DataOriginal, Distancia_entre_puntos):
         DF1.rename(columns={ DF1.columns[0]: "geometry" }, inplace = True) # cambiamos el nombre del campo
         DF = pd.concat([DF, DF1])    # unimos el df anterior con el df general
 
-    DF2 = pd.DataFrame(DataOriginal.loc[DataOriginal.Dist < 2*Div].centroid) # calculamos el centroide para lineas menores a Distancia_entre_puntos
+    DF2 = pd.DataFrame(DataOriginal.loc[DataOriginal.Dist < DMP].centroid) # calculamos el centroide para lineas menores a Distancia_entre_puntos
     DF2.rename(columns={DF2.columns[0]: "geometry" }, inplace = True)      # cambiamos el nombre del campo
     DF3 = pd.concat([DF, DF2])      # unimos el df resultante con el obtenido en la iteración anterior 
     
-    return DF3
+    GDF3 = gpd.GeoDataFrame(DF3, geometry='geometry')
+    GDF3.crs = {'init' :'epsg:4326'}
+    
+    return GDF3
 
 
-# In[19]:
+# In[ ]:
 
 
-# Cargamos EPV como líneas y calculamos la longitud
-LineasEPV = gpd.read_file(PATH + '/EspaciosVerdes_lineas_' + Ciudad + '.shp')
-LineasEPV['Dist'] = LineasEPV.length
-LineasEPV.head(3)
+# Calculamos la longitud y corremos la función
+EspaciosVerdes_lineas_GDF['Dist'] = EspaciosVerdes_lineas_GDF.length
+GDF_Lineas_a_Puntos = Lineas_a_Puntos_Intervalo(EspaciosVerdes_lineas_GDF, 50, 100)
 
 
-# In[20]:
+# In[ ]:
 
 
-# Corremos la función
-DF_Lineas_a_Puntos = Lineas_a_Puntos(LineasEPV, 50)
-
-
-# In[63]:
-
-
-# Pasamos el DF a GDF
-GDF_Lineas_a_Puntos = gpd.GeoDataFrame(DF_Lineas_a_Puntos, geometry='geometry')
-
-
-# In[40]:
-
-
-# Ploteamos para ver el resultado
 fig, ax = plt.subplots(figsize=(15,15))
-LineasEPV.plot(ax=ax, color='red')
-GDF_Lineas_a_Puntos.plot(ax=ax, color='green');
+AreaInteres.plot(ax=ax, color='lightgrey')
+EspaciosVerdes.plot(ax=ax, color='yellow')
+EspaciosVerdes_lineas_GDF.plot(ax=ax, color='green');
+GDF_Lineas_a_Puntos.plot(ax=ax, color='red');
 
 
-# In[29]:
+# In[ ]:
 
 
 GDF_Lineas_a_Puntos.to_file(PATH + '/EspaciosVerdes_puntos_' + Ciudad + '.shp')
 
 
-# # Le asignamos a cada punto un ID de espacio verde
+# ### Le asignamos a cada punto un ID de espacio verde
 
-# In[33]:
-
-
-EspaciosVerdes.crs
+# In[ ]:
 
 
-# In[79]:
-
-
-EspaciosVerdes_SJ = EspaciosVerdes[['leisure', 'name', 'osm_type', 'full_id', 'osm_id', 'geometry']]
+EspaciosVerdes_SJ = EspaciosVerdes.loc[:,['leisure', 'name', 'osm_type', 'full_id', 'osm_id', 'geometry']]
 EspaciosVerdes_SJ['geometry'] = EspaciosVerdes_SJ.geometry.buffer(1.0457242224755355e-05)
 EspaciosVerdes_SJ.head(3)
 
 
-# In[82]:
-
-
-# Ploteamos para ver el resultado
-fig, ax = plt.subplots(figsize=(15,15))
-EspaciosVerdes_SJ.loc[EspaciosVerdes_SJ.full_id == 'r454902'].plot(ax=ax, color='red')
-EspaciosVerdes.loc[EspaciosVerdes.full_id == 'r454902'].plot(ax=ax, color='green');
-
-
-# In[69]:
-
-
-GDF_Lineas_a_Puntos.crs = {'init' :'epsg:4326'}
-
-
-# In[70]:
-
-
-GDF_Lineas_a_Puntos.crs 
-
-
-# In[84]:
+# In[ ]:
 
 
 GDF_Lineas_a_Puntos.reset_index(inplace=True)
 GDF_Lineas_a_Puntos.reset_index(inplace=True)
+GDF_Lineas_a_Puntos.head()
 
 
-# In[87]:
+# In[ ]:
 
 
 GDF_Lineas_a_Puntos_ID = gpd.sjoin(GDF_Lineas_a_Puntos, EspaciosVerdes_SJ, how="left", op='intersects')
@@ -295,39 +301,33 @@ GDF_Lineas_a_Puntos_ID = gpd.sjoin(GDF_Lineas_a_Puntos, EspaciosVerdes_SJ, how="
 
 GDF_Lineas_a_Puntos_ID.rename(columns={ GDF_Lineas_a_Puntos_ID.columns[0]: "ID_Gen" , GDF_Lineas_a_Puntos_ID.columns[1]: "ID_Linea" }, inplace = True)
 GDF_Lineas_a_Puntos_ID.drop(columns = 'index_right', inplace=True)
-GDF_Lineas_a_Puntos.head()
+GDF_Lineas_a_Puntos_ID.head()
 
 
-# In[105]:
+# In[ ]:
 
 
 GDF_Lineas_a_Puntos_ID = GDF_Lineas_a_Puntos_ID.drop_duplicates(subset='ID_Gen', keep="first")
+len(GDF_Lineas_a_Puntos_ID)
 
 
-# In[116]:
+# In[ ]:
 
 
 GDF_Lineas_a_Puntos_ID.to_file(PATH + '/EspaciosVerdes_puntos_ID_' + Ciudad + '.shp')
 
 
-# # Calcular distancias más cercana
+# <a id='section6'></a>
+# 
+# <div style="border-bottom:1px solid #000;"> 
+# <div align="center"><h2>Calcular distancias más cercana</h2></div>
+# <div align="right">(volver a <a href='#indice'><b>Indice</b></a>)</div>
+# </div>
 
-# In[46]:
-
-
-Zonas.head(3)
-
-
-# In[117]:
-
-
-GDF_Lineas_a_Puntos_ID.head(3)
+# In[ ]:
 
 
-# In[118]:
-
-
-EPV_ , EPV_datos = anapos.cercania(Zonas, 'ZONA', 'geometry', GDF_Lineas_a_Puntos_ID, 'ID_Gen', 'geometry', 3, centroid_origen=True, max_distancia=300)
+# EPV_ , EPV_datos = anapos.cercania(Zonas, 'ZONA', 'geometry', GDF_Lineas_a_Puntos_ID, 'ID_Gen', 'geometry', 3, centroid_origen=True, max_distancia=300)
 
 
 # In[ ]:
